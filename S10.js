44cb9885bedfbffc7b4adb1d0112525379a2b66a
alireza10up <toolsalireza10up@gmail.com>
2023-12-12 17:00:31 +0330

    append File

--- S10.js
+++ S10.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const http = require('http');
 const port = 8585;
@@ -5,7 +6,9 @@ const server = http.createServer(rh);
 server.listen(port);
 
 const routes = {
-    fileUpload: fileUpload
+    fileUpload: fileUpload,
+    fileWrite: insertToFile,
+    fileAppend: fileAppend
 }
 
 const headers = {
@@ -23,7 +26,11 @@ function rh(req, res) {
         req.on('data', chunk => {
             data += chunk;
         }).on('end', () => {
-            result = routes[fp](req, data);
+            try {
+                result = routes[fp](req, data);
+            } catch (err) {
+                console.log('Err : ', err);
+            }
         });
     }
 
@@ -36,4 +43,19 @@ function fileUpload(req, data) {
     } catch (error) {
         console.log('Err : ', error);
     }
+}
+
+function insertToFile(req, data) {
+    fs.writeFile('file.txt', data, 'utf-8', err => {
+        if (err) console.log('err : ', err);
+        else console.log('file saved !');
+    });
+}
+
+function fileAppend(req, data) {
+    fs.readFile('file.txt', function (err, dataFile) {
+        let writeData = JSON.parse(dataFile);
+        writeData.data.push(JSON.parse(data));
+        insertToFile(req, JSON.stringify(writeData));
+    });
 }
