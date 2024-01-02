4f33ce05eb764a4d1a42092869d476eb339579c2
alireza10up <toolsalireza10up@gmail.com>
2024-01-02 17:01:42 +0330

    Validation jwt

--- S12.js
+++ S12.js
@@ -46,6 +46,7 @@ Route.post('sign_up', (req, data) => {
         insertToFile.fileAppend(req, data);
     }
 });
+
 Route.post('sign_in', (req, res, data) => {
     // validation
     dataJson = JSON.parse(data);
@@ -71,7 +72,56 @@ Route.post('sign_in', (req, res, data) => {
 
                 if (found) {
                     console.log('user found !');
-                    console.log(jwt.sign(found, 'shhhhhhhhh'));
+                    token = jwt.sign(found, 'arv');
+                    console.log(token);
+                    res.writeHead(200, {
+                        'Set-Cookie': token,
+                        'Content-Type': 'text/plain',
+                    });
+                    write(res, 'user found !', 'text');
+                } else {
+                    console.log('user not found !');
+                    write(res, 'user not found !', 'text');
+                }
+            }
+        });
+    }
+});
+
+Route.get('cookie', (req, res, data) => {
+    console.log(req.headers.cookie);
+    decodeData = jwt.verify(req.headers.cookie, 'arv');
+
+    dataJson = decodeData;
+    console.log(dataJson);
+    if (dataJson.name === undefined || dataJson.email === undefined || dataJson.pass === undefined) {
+        console.log('Data Not Valid');
+
+        write(res, 'Data Not Valid', 'text');
+    } else {
+        fs.readFile('database.txt', 'utf-8', function (err, dataFile) {
+            if (err) {
+                console.log('Data Not Valid');
+                write(res, 'file not found !', 'text');
+            } else {
+                dataFile = JSON.parse(dataFile);
+
+                let found = false;
+
+                dataFile.data.forEach(item => {
+                    if (item.email == dataJson.email && item.pass == dataJson.pass) {
+                        found = item;
+                    }
+                });
+
+                if (found) {
+                    console.log('user found !');
+                    token = jwt.sign(found, 'arv');
+                    console.log(token);
+                    res.writeHead(200, {
+                        'Set-Cookie': token,
+                        'Content-Type': 'text/plain',
+                    });
                     write(res, 'user found !', 'text');
                 } else {
                     console.log('user not found !');
@@ -101,10 +151,10 @@ function rh(req, res) {
         try {
             result = Route.execute(fp, method);
             if (result instanceof Function) {
-                //  res.writeHead(200, headers.text);
+                res.writeHead(200, headers.text);
                 let remoteDataPromise = result(req, res, data);
                 // console.log(remoteDataPromise);
-                // res.write(remoteDataPromise.data ? remoteDataPromise : remoteDataPromise.data);
+                // res.write(remoteDataPromise.data !== undefined ? remoteDataPromise : remoteDataPromise.data);
                 // res.end();
             } else {
                 res.write(result);
@@ -112,12 +162,11 @@ function rh(req, res) {
         } catch (err) {
             console.log('Err : ', err);
         }
-        res.end();
     });
 }
 
 function write(res, data, status) {
-    res.writeHead(200, headers.text);
+    // res.writeHead(200, headers.text);
     res.write(data);
     res.end();
 }
