16d79ab51abf49738b5f41df90dfd33ce786c127
alireza10up <toolsalireza10up@gmail.com>
2024-01-19 22:42:55 +0330

    implemented router

--- notebook-project/index.js
+++ notebook-project/index.js
@@ -2,3 +2,11 @@
 * routes (login , register , lists)
 * request handler (validation)
 */
+
+// libs
+const fs = require('fs');
+const router = require('./core/router');
+
+// register routes
+
+// request handler
