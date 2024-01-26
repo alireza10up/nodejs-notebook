d287c59558368faeb1c70c85e201c7920ff03061
alireza10up <toolsalireza10up@gmail.com>
2024-01-26 16:48:37 +0330

    Fixes Bug In Linux Os

--- notebook-project/core/utilities/index.js
+++ notebook-project/core/utilities/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 class Utilities {
 	static assets(path) {
-		path = __dirname+'\\..\\..\\resource\\templates\\'+path;
+		path = __dirname+'/../../resource/templates//'+path;
 		return fs.readFileSync(path, (err, data) => {
 			if (err) {
 				return Utilities.error();
@@ -12,7 +12,7 @@ class Utilities {
 	}
 
 	static template(filename) {
-		let path = __dirname+'\\..\\..\\resource\\templates\\'+filename+'.html';
+		let path = __dirname+'/../../resource/templates/'+filename+'.html';
 		return fs.readFileSync(path, 'utf-8', (err, data) => {
 			if (err) {
 				return Utilities.error();
@@ -22,7 +22,7 @@ class Utilities {
 	}
 
 	static page404() {
-		let path = __dirname+'\\..\\..\\resource\\templates\\404.html';
+		let path = __dirname+'/../../resource/templates/404.html';
 		return fs.readFileSync(path, (err, data) => {
 			if (err) {
 				return Utilities.error();
@@ -32,7 +32,7 @@ class Utilities {
 	}
 
 	static error() {
-		let path = __dirname+'\\..\\..\\resource\\templates\\error.html';
+		let path = __dirname+'/../../resource/templates/error.html';
 		return fs.readFileSync(path, (err, data) => {
 			if (err) {
 				return 'Error Serve !';
